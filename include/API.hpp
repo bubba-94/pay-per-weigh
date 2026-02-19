ca47eb4252a17f2cf195ac5864c359de454a1721
bubba-94 <carljohan.modin@gmail.com>
2026-02-19 15:35:07 +0100

    build: Compilation sucessful.
    - Changed the QR code.
    - Certificates needed for SSL instances

--- include/API.hpp
+++ include/API.hpp
@@ -1,6 +1,7 @@
 #ifndef API_HPP
 #define API_HPP
 
+#define CPPHTTPLIB_OPENSSL_SUPPORT // SSL Server support for httplib.h
 #include "external-libs/httplib.h"
 
 #include <cstdint>
@@ -9,9 +10,32 @@
 #include <thread>
 #include <vector>
 
+/*
+curl -v -X POST https://mpc.getswish.net/qrg-swish/api/v1/prefilled \
+-H "Content-Type: application/json" \
+--output assets/img/qr.png \
+--data '{
+    "format": "png",
+    "payee": {
+        "value": "0767639956",
+        "editable": false
+    },
+    "amount": {
+        "value": 1,
+        "editable": false
+    },
+    "message": {
+        "value": "Pay-per-weigh",
+        "editable": false
+    },
+    "size": 300,
+    "border" : 2,
+    "transparent" : true
+}'
+*/
+
 /***
- *
- * @class
+ *img
  * SwishClient
  *
  * @brief
@@ -19,9 +43,20 @@
  */
 class SwishClient {
 public:
+  SwishClient(const std::string &client, const std::string &base_url, int port);
+  int startPaymentProcess();
+
+  bool checkSucessfulPayment();
+
 private:
-  httplib::Client client;
+  bool paymentSuccessful = false;
+
+  httplib::SSLClient client; // Interact with endpoints
+  httplib::SSLServer server; // Serve endpoints
+
   httplib::Request req;
+  httplib::Params params; // Possibility of making requests with params.
+
   httplib::Response res;
 };
 
